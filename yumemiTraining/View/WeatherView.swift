991a87afefd84368800259fe4dfa43ef3f10028c
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-04-22 23:23:22 +0900

    Viewの作成、ViewControllersyuusei

--- yumemiTraining/View/WeatherView.swift
+++ yumemiTraining/View/WeatherView.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class WeatherView: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    
+    
+    
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            loadNib()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        private func loadNib() {
+          let nib = UINib(nibName: "WeatherView", bundle: nil)
+            if let view = nib.instantiate(withOwner: self, options: nil).first as? UIView {
+                
+                view.frame = self.bounds
+                self.addSubview(view)
+            }
+        }
+}
